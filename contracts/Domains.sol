d20c40a4de86806e67e1badb44befdd25ee33890
FranLpz <franciscoalopezd@gmail.com>
2022-05-01 08:52:05 -0500

    Update the domains contract to handle payments

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,25 +1,63 @@
 // SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
-  // A "mapping" data type to store their names
+  // Here's our domain TDL!
+  string public tld;
+
+  // Our "mapping" store for domains and records
   mapping(string => address) public domains;
+  mapping(string => string) public records;
   
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+  // We make the contract "payable" by adding this to the constructor
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
   }
 
-  // A register function that adds their names to our mapping
-  function register(string calldata name) public {
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if(len == 3) {
+      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if(len == 4){
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else{
+      return 1 * 10**17;
+    }
+  }
+
+  // A register function that adds their names to our mapping NOW PAYABLE
+  function register(string calldata name) public payable {
+    // Check that the name is unregistered (explained in notes)
+    require(domains[name] == address(0));
+
+    uint _price = price(name);
+
+    // Check it enough Matic was paid in the transaction
+    require(msg.value >= _price, "Not enough Matic paid");
+
     domains[name] = msg.sender;
     console.log("%s has registered a domain!", msg.sender);
   }
 
   // This will give us the domain owners' address
   function getAddress(string calldata name) public view returns (address) {
+      // Check that the owner is the transaction sender
       return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+    // Check that the owner is the transaction sender
+    require(domains[name] == msg.sender);
+    records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+    return records[name];
   }
 }
